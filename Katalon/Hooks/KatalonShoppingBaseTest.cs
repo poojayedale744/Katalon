b3cc2f026bdca574b55c00b2c5a4c635d996d9d1
Pooja Yedale <poojayedale744@gmail.com>
2023-05-12 19:16:49 +0100

    Added hooks for base test

---
+++ Katalon/Hooks/KatalonShoppingBaseTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+using TechTalk.SpecFlow;
+
+namespace ProjectTask.Hooks
+{
+    [Binding]
+    public class KatalonShoppingBaseTest
+    {
+        public static IWebDriver driver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://cms.demo.katalon.com/");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(05);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+    }
+}
